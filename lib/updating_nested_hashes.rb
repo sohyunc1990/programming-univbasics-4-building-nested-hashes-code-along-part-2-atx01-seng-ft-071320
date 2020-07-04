89b10e76977019b47978036d520c9b379ed18c24
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-04 19:57:02 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -67,6 +67,10 @@ def add_characters
   epic_tragedy[:additional_characters] = []
   epic_tragedy[:additional_characters][0] = { name: "Prince Escalus" }
   epic_tragedy[:additional_characters][1] = { name: "Apothecary" }
+  epic_tragedy[:additional_characters] = [
+  { name: "Prince Escalus" },
+  { name: "Apothecary" }
+]
 
 end
 
