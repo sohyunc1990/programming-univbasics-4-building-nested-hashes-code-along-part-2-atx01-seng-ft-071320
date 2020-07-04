3d9951924696ed05a201c519757dc6049a2ac54c
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-04 19:41:14 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -29,7 +29,7 @@ def update_status
 
   # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
   
-  epic_tragedy
+  puts epic_tragedy
   
 end
 
